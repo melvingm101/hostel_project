e87eccb092872ee845fdf4928d78868238e5a285
Ahmad Uzair <ahmad.uzair@studentpartner.com>
2019-05-11 11:20:25 +0400

    - Implemented Student model only - Using model instead of static SQL - Homepage gets the data from Student objects

--- students/models.py
+++ students/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Other_Charges(models.Model):
+    charge_name=models.CharField(max_length=50)
+    charge_value=models.FloatField()
+
+
+class Charges(models.Model):
+    breakage_charges=models.FloatField()
+    laundry_charges=models.FloatField()
+    lost_key_charges=models.FloatField()
+    other_charges=models.ForeignKey(Other_Charges,on_delete=models.CASCADE)
+    guest_charges=models.FloatField()
+
+
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    id_no=models.CharField(max_length=20)
+    room_no=models.IntegerField()
+    joining_date=models.DateField()
+    leaving_date=models.DateField()
+    block=models.CharField(max_length=1)
+    #charges=models.ForeignKey(Charges,on_delete=models.CASCADE)
+    mob_no=models.CharField(max_length=20)
+    email=models.CharField(max_length=100)
+    date_added=models.DateTimeField()
+
--- students/views.py
+++ students/views.py
@@ -6,26 +6,27 @@ from django import forms
 from django.shortcuts import render
 from students.forms import LoginForm
 from django.contrib.auth.decorators import login_required
-
+from students.models import Student
+import datetime
 @login_required()
 def front_page(request):
     normal = []
     if request.method == "POST" and request.FILES['excel_file']:
-        connection = sqlite3.connect('students.db')
-        cursor = connection.cursor()
-
-        print("Table created!")
-        sql_create = """ CREATE TABLE IF NOT EXISTS students(
-            NUMBER integer PRIMARY KEY AUTOINCREMENT,
-            ROOMNO VARCHAR(4),
-            BLOCK VARCHAR(3),
-            STUDENT VARCHAR(30),
-            MOBILE VARCHAR(20),
-            ID VARCHAR(20)
-        ) """
-
-        cursor.execute(sql_create)
-        print("Table created!")
+        # connection = sqlite3.connect('students.db')
+        # cursor = connection.cursor()
+        #
+        # print("Table created!")
+        # sql_create = """ CREATE TABLE IF NOT EXISTS students(
+        #     NUMBER integer PRIMARY KEY AUTOINCREMENT,
+        #     ROOMNO VARCHAR(4),
+        #     BLOCK VARCHAR(3),
+        #     STUDENT VARCHAR(30),
+        #     MOBILE VARCHAR(20),
+        #     ID VARCHAR(20)
+        # ) """
+        #
+        # cursor.execute(sql_create)
+        # print("Table created!")
 
 
         #sql_insert = """ INSERT INTO students VALUES("312", "A", "Melvin George Mathew", "0509940039", "2015A7PS0197U")"""
@@ -34,39 +35,46 @@ def front_page(request):
 
         excel_files = request.FILES["excel_file"]
         wb = openpyxl.load_workbook(excel_files)
-        worksheet = wb["Book1"]
-        #print(worksheet.get_highest_column())
-        for row in worksheet.rows:
-            normal.append(row)
-        for rows in normal:
-            print(rows[1].value)
-            sql_insert = "INSERT INTO students(ROOMNO, BLOCK, STUDENT, MOBILE, ID) VALUES('{0}', '{1}', '{2}', '{3}', '{4}')".format(rows[0].value, rows[1].value, rows[2].value, rows[3].value, rows[4].value)
-            cursor.execute(sql_insert)
-            print("Data added!")
+        worksheet = wb["Sheet1"]
+        # #print(worksheet.get_highest_column())
+        # for row in worksheet.rows:
+        #     normal.append(row)
+        # for rows in normal:
+        #     print(rows[1].value)
+        #     sql_insert = "INSERT INTO students(ROOMNO, BLOCK, STUDENT, MOBILE, ID) VALUES('{0}', '{1}', '{2}', '{3}', '{4}')".format(rows[0].value, rows[1].value, rows[2].value, rows[3].value, rows[4].value)
+        #     cursor.execute(sql_insert)
+        #     print("Data added!")
+        #
+        # cursor.execute("SELECT * FROM students")
+        # res = cursor.fetchall()
+        # connection.commit()
+        # connection.close()
+        # return render(request, 'homepage.html', {'res': res})
 
-        cursor.execute("SELECT * FROM students")
-        res = cursor.fetchall()
-        connection.commit()
-        connection.close()
-        return render(request, 'homepage.html', {'res': res})
-
-    connection = sqlite3.connect('students.db')
-    cursor = connection.cursor()
-    sql_create = """ CREATE TABLE IF NOT EXISTS students(
-                NUMBER integer PRIMARY KEY AUTOINCREMENT,
-                ROOMNO VARCHAR(4),
-                BLOCK VARCHAR(3),
-                STUDENT VARCHAR(30),
-                MOBILE VARCHAR(20),
-                ID VARCHAR(20)
-            ) """
-
-    cursor.execute(sql_create)
-    cursor.execute("SELECT * FROM students")
-    res = cursor.fetchall()
-    connection.commit()
-    connection.close()
-    return render(request, 'homepage.html', {'res': res})
+        #connection = sqlite3.connect('students.db')
+        #cursor = connection.cursor()
+        # sql_create = """ CREATE TABLE IF NOT EXISTS students(
+        #             NUMBER integer PRIMARY KEY AUTOINCREMENT,
+        #             ROOMNO VARCHAR(4),
+        #             BLOCK VARCHAR(3),
+        #             STUDENT VARCHAR(30),
+        #             MOBILE VARCHAR(20),
+        #             ID VARCHAR(20)
+        #         ) """
+        #
+        # cursor.execute(sql_create)
+        stud_rows=list(worksheet.rows)
+        for row in stud_rows[1:]:
+            args=[cell.value for cell in row]
+            student=Student(name=args[0],id_no=args[1],room_no=args[2],joining_date=args[3],leaving_date=args[4],block=args[5],mob_no=args[6],email=args[7],date_added=datetime.datetime.now())
+            student.save()
+        # cursor.execute("SELECT * FROM students")
+        # res = cursor.fetchall()
+        # connection.commit()
+        # connection.close()
+        # return render(request, 'homepage.html', {'res': res})
+    res=Student.objects.all()
+    return render(request, 'homepage.html',{'res':res})
 
 
 def login_page(request):
@@ -79,14 +87,16 @@ def insert_excel(request):
     cursor = connection.cursor()
     if request.method == "POST":
         data = request.POST.copy()
-        room_no = data.get('roomNo')
-        block = data.get('blockNo')
-        student = data.get('studentName')
-        mobile = data.get('mobileNo')
-        id = data.get('studentID')
-        print(room_no)
-        sql_insert_single = "INSERT INTO students(ROOMNO, BLOCK, STUDENT, MOBILE, ID) VALUES('{0}', '{1}', '{2}', '{3}', '{4}')".format(room_no, block, student, mobile, id)
-        cursor.execute(sql_insert_single)
+        student=Student()
+        student.room_no = data.get('room_no')
+        student.block = data.get('block')
+        student.name = data.get('name')
+        student.mob_no = data.get('mob_no')
+        student.id_no = data.get('id_no')
+        print(student.room_no)
+        student.save()
+        #sql_insert_single = "INSERT INTO students(ROOMNO, BLOCK, STUDENT, MOBILE, ID) VALUES('{0}', '{1}', '{2}', '{3}', '{4}')".format(room_no, block, student, mobile, id)
+        #cursor.execute(sql_insert_single)
         connection.commit()
         connection.close()
     return render(request, 'insertExcel.html')
